fcb87c7a90204f8dfa0113ed4eebc5a271fdd561
Jakub Jakubowski <jakubjakubowski8@gmail.com>
2021-08-17 22:03:47 +0200

    Fix docker-compose

--- start_dev.sh
+++ start_dev.sh
@@ -3,6 +3,7 @@
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://34.207.216.185:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials/dev_env"
+export COMPOSE_INTERACTIVE_NO_CLI=1
 
 /usr/bin/docker-compose -f docker-compose-non-dev.yml kill
 /usr/bin/docker-compose -f docker-compose-non-dev.yml down
